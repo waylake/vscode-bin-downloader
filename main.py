0dbfafcd5a195a6da5393b421a3d155b040eb824
waylake <hwangdoyeonyu@gmail.com>
2022-10-11 09:19:50 +0900

    make download function that vscode-server bin

---
+++ main.py
@@ -0,0 +1,20 @@
+import wget
+import os
+
+BASE_URL = "https://update.code.visualstudio.com/commit:"
+LAST_URL = "/server-linux-x64/stable"
+
+
+def download_vscode_bin(commit_id):
+    dirs = [".vscode-server", f".vscode-server/{commit_id}"]
+    for dir in dirs:
+        if not os.path.exists(dir):
+            os.mkdir(dir)
+
+    url = BASE_URL + commit_id + LAST_URL
+    path = f".vscode-server/{commit_id}/"
+    file_name = "vscode-server.tar.gz"
+    wget.download(url=url, out=path + file_name)
+
+
+download_vscode_bin("64bbfbf67ada9953918d72e1df2f4d8e537d340e")
